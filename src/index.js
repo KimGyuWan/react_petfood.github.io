85afbe16e80f8499e8eeb2b4b0ee42135629f21f
KimGyuWan <y5g5776@naver.com>
2023-12-13 16:54:53 +0900

    itemsection 추출완료

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import Item from './component/gyuwan/Iitem';
-import Instagram from './component/gyuwan/Instagram'
+import './common.css';
+
+import Item from './component/gyuwan/Item';
+import Instagram from './component/gyuwan/Instagram';
+
+import data from './data/data.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+    <App />
+    <Item dataMain={data.item_mainlist} dataList={data.item_listgroup} />
+    <Instagram />
+  </>
 );
 
 
