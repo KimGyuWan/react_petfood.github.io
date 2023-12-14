e2f35e887c4d9aa7250c42205bcb182610d03d9f
User -2023WJDST\Administrator <dheltus1r@naver.com>
2023-12-14 18:07:23 +0900

    brabd 작업

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Brand from './component/dongmin/Brand';
+import datainfo from './data/data.json'
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+    <Brand data={datainfo}></Brand>
+
+  </>
+
 );
 
 
